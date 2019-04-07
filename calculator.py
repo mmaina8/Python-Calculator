eb0fe3c10ddba9ef408f7658df02e99a69d2285a
mmaina8 <47848464+mmaina8@users.noreply.github.com>
2019-04-07 14:27:56 +0300

    Added new file

---
+++ calculator.py
@@ -0,0 +1,29 @@
+def add (a,b):
+	answer = a+b
+	return answer
+
+def subtract (a,b):
+	answer= a-b
+	return answer
+
+def multiply (a,b):
+	answer= a*b
+	return answer
+
+def divide (a,b):
+	answer= a/b
+	return answer
+
+def modulus(a,b):
+	answer= a%b
+	return answer
+
+def whole_number_division(a,b):
+	answer= a//b
+	return answer
+def exponent(a,b):
+	answer = a**b
+	return answer
+def percentage(a,b):
+	answer = (a/b)*100
+	return answer
